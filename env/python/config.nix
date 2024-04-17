b09bc861971240e487f8ec20eb5d2628948ff62e
Clément <clement2104.boillot@gmail.com>
2024-04-17 02:06:02 +0200

    Add pyxel config target

--- env/python/config.nix
+++ env/python/config.nix
@@ -2,8 +2,6 @@
   home-manager.users.guest = {
     home.packages = with pkgs; [
       python311
-      python311.pkgs.pip
-      python311.pkgs.venv
       python311.pkgs.pillow
       python311.pkgs.pygame
 
